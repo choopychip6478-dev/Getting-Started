5755859b0cc1d7b9e28cf14ea69a26540a3d829e
choopychip6478-dev <choopychip.6478@gmail.com>
2026-09-10 22:06:13 +0000

    Submit: cpp/03-reverse-n

--- assignments/cpp/03-reverse-n/main.cpp
+++ assignments/cpp/03-reverse-n/main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    // Write your solution here.
+    int n;
+    cin >> n;
+    int splat[n];
+    for (int i = 0; i < n; ++i)
+        cin >> splat[i];
+    for (int i = n-1; i >= 0; --i)
+        cout << splat[i] << " ";
     return 0;
 }
