546db33715c64da0ff3260c56b86dd7698384aca
choopychip6478-dev <choopychip.6478@gmail.com>
2026-09-15 22:07:36 +0000

    Submit: cpp/05-sum-n

--- assignments/cpp/05-sum-n/main.cpp
+++ assignments/cpp/05-sum-n/main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    int o1,t2,tr3,f4,fi5;
-    cin >> o1 >> t2 >> tr3 >> f4 >> fi5;
-    cout << o1+t2+tr3+f4+fi5;
+    long long o1,t2;
+    cin >> o1 >> t2;
+    for (int i = 1, t3; i < o1; ++i) cin >> t3, t2 += t3;
     return 0;
 }
